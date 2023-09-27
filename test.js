e2b82e5063fc82402ef5747eb1c68c6a8a07829e
Zach Gentner <zach.gentner@gmail.com>
2023-09-27 14:16:19 -0400

    init

---
+++ test.js
@@ -0,0 +1,32 @@
+const test = document.getElementById('test');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+const key = 'zVx0IcmoqVOG71GGd4U9gnjNiLTOf2I1';
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    getGif();
+})
+
+function getGif() {
+    let term = input.value;
+    input.value = "";
+    let query = `https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${term}`;
+
+    if (term !== '') {
+        fetch(query, {mode: 'cors'})
+            .then(response => {
+                return response.json();
+            })
+            .then(response => {
+                console.log(response.data.images.original.url);
+                let source = response.data.images.original.url;
+                test.children[0].setAttribute('src', source)
+            })
+            .catch(error => {
+                console.log(error)                
+            });
+    }
+}
